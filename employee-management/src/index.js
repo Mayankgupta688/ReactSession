18d0130b42c27478aca4deeaab7131da947c87d6
MayankGupta <mayank.gupta.6.88@gmail.com>
2019-09-17 18:13:09 +0530

    Changes

--- employee-management/src/index.js
+++ employee-management/src/index.js
@@ -1,16 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-let displayTemplate = (
-    <div id="outer">
-        <div>
-            <b>
-                <div>User Data: 1</div>
-                <div>User Data: 2</div>
-            </b>
-    </div></div>
-);
+var otherName = "Mayank Gupta";
 
-ReactDOM.render(displayTemplate, document.getElementById("root"));
+var color = "red";
 
-ReactDOM.render(<h4>This is other Part of Application</h4>, document.getElementById("otherDiv"));
+var footerStyle = {
+    "color": color,
+    "backgroundColor": "green",
+    "paddingLeft": "50px"
+}
+
+var applicationStyling = {
+    "header": {
+        "color": "green"
+    }, "footer": {
+        "color": "red"
+    }, "content": {
+        "color": "yellow"
+    }
+}
+
+
+function HeaderComponent() {
+    return <h1 style={applicationStyling.header}>This is Header Page</h1>
+}
+
+function ContentPage(props) {
+    props.details.name = "sgfisdgfisfg";
+    return <h2 style={applicationStyling.content}>This is Content Part... for { "Mayank".indexOf("nk") }... Aged {props.age}</h2>
+}
+
+function FooterComponent() {
+    return <h1 style={applicationStyling.footer}>This is Footer Page</h1>
+}
+
+var userDetails = {
+    name: "Mayank",
+    age: "20"
+}
+
+ReactDOM.render(<div>
+    <HeaderComponent></HeaderComponent>
+    <ContentPage name="Mayank" age="10" details={userDetails}></ContentPage>
+    <ContentPage name="Anshul" details={userDetails}></ContentPage>
+    <FooterComponent></FooterComponent>
+</div>, document.getElementById("root"));
